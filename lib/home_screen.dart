528917f6ef578022ef3133da2db0af1d60a3bc6b
TylerHight <@pplePotat0>
2023-09-29 03:36:36 -0500

    Added emission duration and periodic emissions sliders

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'power_button.dart'; // Import the PowerButton widget
 import 'emission_button.dart'; // Import the EmissionButton widget
+import 'settings.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double emissionDuration = EmissionDurationSettings.defaultDuration;
+  double periodicEmissionFrequency = PeriodicEmissionSettings.defaultDuration;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -16,7 +25,39 @@ class HomeScreen extends StatelessWidget {
         ),
         Expanded(
           child: Center(
-            child: EmissionButton(),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                EmissionButton(),
+                SizedBox(height: 20), // Add spacing between EmissionButton and sliders
+                Slider( // Emission Duration Slider
+                  value: emissionDuration,
+                  min: EmissionDurationSettings.min,
+                  max: EmissionDurationSettings.max,
+                  divisions: EmissionDurationSettings.divisions,
+                  onChanged: (double value) {
+                    setState(() {
+                      emissionDuration = value;
+                    });
+                  },
+                  // display the selected value
+                  label: emissionDuration.toStringAsFixed(1),
+                ),
+                Slider( // Periodic Emission Frequency Slider
+                  value: periodicEmissionFrequency,
+                  min: PeriodicEmissionSettings.min,
+                  max: PeriodicEmissionSettings.max,
+                  divisions: PeriodicEmissionSettings.divisions,
+                  onChanged: (double value) {
+                    setState(() {
+                      periodicEmissionFrequency = value;
+                    });
+                  },
+                  // display the selected value
+                  label: periodicEmissionFrequency.toStringAsFixed(1),
+                ),
+              ],
+            ),
           ),
         ),
       ],
