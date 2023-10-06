fe10fda1d1378cd15cc1ded028502d3cdc03803d
TylerHight <@pplePotat0>
2023-10-06 14:48:41 -0500

    merged side branch with main branch

--- lib/device_remote.dart
+++ lib/device_remote.dart
@@ -1,3 +1,5 @@
+// device_remote.dart
+
 import 'package:flutter/material.dart';
 import 'package:que_app/device_settings.dart';
 import 'binary_button.dart';
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,9 @@
+// home_screen.dart
+
 import 'package:flutter/material.dart';
 import 'device_remote.dart';
-import 'device_settings.dart'; // Import the DeviceSettingsScreen
+import 'device_settings.dart';
+import 'device_name_dialog.dart'; // Import the new dialog file
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,81 +11,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final List<String> deviceNames = []; // Create a list to store device names
+  final List<String> deviceTitles = List.generate(3, (index) => 'Item $index');
 
-  @override
-  void initState() {
-    super.initState();
-    // Initialize the list with some default device names
-    deviceNames.addAll(List.generate(3, (index) => 'Item $index'));
-  }
-
-  void addDevice() {
+  void addDevice(BuildContext context) {
     showDialog(
       context: context,
-      builder: (BuildContext context) {
-        String newDeviceName = ""; // Store the entered device name
-
-        return AlertDialog(
-          title: Text('Add New Device'),
-          content: TextField(
-            onChanged: (value) {
-              newDeviceName = value; // Update the entered device name
-            },
-            decoration: InputDecoration(labelText: 'Device Name'),
-          ),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () {
-                if (newDeviceName.isNotEmpty) {
-                  // Add the new device name to the list
-                  setState(() {
-                    deviceNames.add(newDeviceName);
-                  });
-                  Navigator.of(context).pop(); // Close the dialog
-                }
-              },
-              child: Text('OK'),
-            ),
-          ],
-        );
-      },
+      builder: (context) => DeviceNameDialog(
+        onDeviceNameEntered: (deviceName) {
+          setState(() {
+            deviceTitles.add(deviceName);
+          });
+        },
+      ),
     );
   }
 
-  void deleteDevice(String deviceName) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Delete Device'),
-          content: Text('Delete device $deviceName?'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('No'),
-            ),
-            TextButton(
-              onPressed: () {
-                // Remove the deviceName from the deviceNames list
-                setState(() {
-                  deviceNames.remove(deviceName);
-                });
-                Navigator.of(context).pop(); // Close the dialog
-              },
-              child: Text('Yes'),
-            ),
-          ],
-        );
-      },
+  void deleteDevice(int index) {
+    setState(() {
+      deviceTitles.removeAt(index);
+    });
+  }
+
+  void navigateToDeviceSettings(int index) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DeviceSettingsScreen(
+          onDelete: () {
+            deleteDevice(index);
+            Navigator.pop(context); // Close the settings screen
+          },
+        ),
+      ),
     );
   }
 
@@ -92,44 +52,33 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text(
           'Devices',
-          style: TextStyle(color: Colors.white), // Set title text color
+          style: TextStyle(color: Colors.white),
         ),
         centerTitle: true,
-        backgroundColor: Colors.blue, // Set the background color
-        iconTheme: IconThemeData(color: Colors.black), // Set icon color to black
-        elevation: 4.0, // Set elevation to alter shadow
+        backgroundColor: Colors.blue,
+        iconTheme: IconThemeData(color: Colors.black),
+        elevation: 4.0,
         actions: <Widget>[
           IconButton(
             icon: Icon(
               Icons.add,
-              color: Colors.white, // Set the plus icon color to white
+              color: Colors.white,
             ),
-            onPressed: addDevice, // Call the addDevice function when pressed
+            onPressed: () {
+              addDevice(context);
+            },
           ),
         ],
       ),
       body: ListView.builder(
-        itemCount: deviceNames.length,
+        itemCount: deviceTitles.length,
         itemBuilder: (context, index) {
           return Padding(
-            padding: EdgeInsets.all(2.0), // Add spacing between cards
+            padding: EdgeInsets.all(2.0),
             child: DeviceRemote(
-              title: deviceNames[index], // Pass the title from the list
-              onDelete: () {
-                // Show the DeviceSettingsScreen and provide the deviceName
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DeviceSettingsScreen(
-                      deviceName: deviceNames[index], // Provide the deviceName
-                      onDelete: () {
-                        // Implement your logic to delete the device here
-                        deleteDevice(deviceNames[index]);
-                        // Now you can delete the device using the provided deviceName
-                      },
-                    ),
-                  ),
-                );
+              title: deviceTitles[index],
+              onTap: () {
+                navigateToDeviceSettings(index);
               },
             ),
           );
