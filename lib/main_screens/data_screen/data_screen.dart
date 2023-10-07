9737ed52a2cdfce4079b823a3be39b81cfe061f9
TylerHight <highttyler@gmail.com>
2023-10-06 22:22:29 -0500

    moved the hours input box to the bottom right

--- lib/main_screens/data_screen/data_screen.dart
+++ lib/main_screens/data_screen/data_screen.dart
@@ -18,6 +18,13 @@ class _DataScreenState extends State<DataScreen> {
 
   @override
   Widget build(BuildContext context) {
+    double minY = heartRateData.values.reduce((min, val) => val < min ? val : min).toDouble();
+    double maxY = heartRateData.values.reduce((max, val) => val > max ? val : max).toDouble();
+
+    // Calculate minY and maxY rounding to the nearest multiple of 5
+    minY = (minY / 5).floor() * 5;
+    maxY = ((maxY + 4) / 5).ceil() * 5;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Heartrate and Device Data'),
@@ -25,11 +32,51 @@ class _DataScreenState extends State<DataScreen> {
       ),
       body: Column(
         children: [
+          Expanded(
+            child: Center(
+              child: Row(
+                children: [
+                  // Y-axis title
+                  Container(
+                    margin: EdgeInsets.only(left: 10, right: 0),
+                    child: RotatedBox(
+                      quarterTurns: -1, // Rotate counterclockwise
+                      child: Text(
+                        'Heart Rate (BPM)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                  // Chart
+                  Expanded(
+                    child: SfCartesianChart(
+                      primaryXAxis: DateTimeAxis(),
+                      primaryYAxis: NumericAxis(
+                        minimum: minY, // Set the minimum value of the y-axis
+                        maximum: maxY, // Set the maximum value of the y-axis
+                        interval: 5, // Set the interval to 5 to show labels in multiples of 5
+                      ),
+                      series: <ChartSeries>[
+                        LineSeries<MapEntry<DateTime, double>, DateTime>(
+                          dataSource: heartRateData.entries.toList(),
+                          xValueMapper: (MapEntry<DateTime, double> entry, _) => entry.key,
+                          yValueMapper: (MapEntry<DateTime, double> entry, _) => entry.value,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.end, // Align to the right
               children: [
-                const Text('Show most recent '),
+                const Text('hours: '),
                 SizedBox(
                   width: 60,
                   height: 50,
@@ -45,25 +92,7 @@ class _DataScreenState extends State<DataScreen> {
                     ),
                   ),
                 ),
-                const Text(' hrs'),
               ],
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: SfCartesianChart(
-                primaryXAxis: DateTimeAxis(),
-                primaryYAxis: NumericAxis(
-                  interval: 5, // Set the interval to 5 to show labels in multiples of 5
-                ),
-                series: <ChartSeries>[
-                  LineSeries<MapEntry<DateTime, double>, DateTime>(
-                    dataSource: heartRateData.entries.toList(),
-                    xValueMapper: (MapEntry<DateTime, double> entry, _) => entry.key,
-                    yValueMapper: (MapEntry<DateTime, double> entry, _) => entry.value,
-                  ),
-                ],
-              ),
             ),
           ),
         ],
