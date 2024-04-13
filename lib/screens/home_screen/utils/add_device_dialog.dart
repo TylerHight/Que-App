1d4fbc8c8027edd260d40d3c65b4e6bd5f7805b2
TylerHight <highttyler@gmail.com>
2024-04-13 00:51:26 -0500

    filtered nameless devices

--- lib/screens/home_screen/utils/add_device_dialog.dart
+++ lib/screens/home_screen/utils/add_device_dialog.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class DeviceNameDialog extends StatefulWidget {
   final Function(String) onDeviceNameEntered;
 
-  const DeviceNameDialog({Key? key, required this.onDeviceNameEntered}) : super(key: key);
+  const DeviceNameDialog({Key? key, required this.onDeviceNameEntered})
+      : super(key: key);
 
   @override
   _DeviceNameDialogState createState() => _DeviceNameDialogState();
@@ -12,73 +14,52 @@ class DeviceNameDialog extends StatefulWidget {
 
 class _DeviceNameDialogState extends State<DeviceNameDialog> {
   final TextEditingController _deviceNameController = TextEditingController();
-  BluetoothDevice? _selectedDevice;
-  List<ScanResult> _scanResults = []; // Store scan results
-
-  @override
-  void initState() {
-    super.initState();
-    _startScan();
-  }
-
-  @override
-  void dispose() {
-    FlutterBlue.instance.stopScan();
-    super.dispose();
-  }
-
-  void _startScan() {
-    FlutterBlue.instance.startScan();
-    FlutterBlue.instance.scanResults.listen((results) {
-      setState(() {
-        _scanResults = results;
-      });
-    });
-  }
+  String _selectedDevice = ''; // Default selected device
+  List<ScanResult> _availableDevices = [];
+  bool _isScanning = false;
 
   @override
   Widget build(BuildContext context) {
+    // Filter devices to remove unknown devices or devices without names
+    List<ScanResult> filteredDevices = _availableDevices
+        .where((device) => device.device.name.isNotEmpty)
+        .toList();
+
     return AlertDialog(
       title: const Text('Add New Device'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          // Display a dropdown menu of available Bluetooth devices
-          DropdownButtonFormField<ScanResult>(
-            value: _selectedDevice != null
-                ? _scanResults.firstWhere(
-                  (result) => result.device.id == _selectedDevice!.id,
-              orElse: () => _scanResults.isNotEmpty ? _scanResults[0] : _scanResults.first,
-            )
-                : _scanResults.isNotEmpty ? _scanResults[0] : null,
-            items: _scanResults.isNotEmpty
-                ? _scanResults.map((result) {
-              return DropdownMenuItem<ScanResult>(
-                value: result,
-                child: Text(result.device.name ?? 'Unknown Device'),
-              );
-            }).toList()
-                : [
-              DropdownMenuItem<ScanResult>(
-                value: null,
-                child: Text('No devices found'),
-              ),
-            ],
-            onChanged: _scanResults.isNotEmpty
-                ? (result) {
-              setState(() {
-                _selectedDevice = result!.device;
-              });
-            }
-                : null,
-            hint: _scanResults.isEmpty
-                ? const Text('No devices found')
-                : const Text('Select Device (Optional)'),
-          ),
-          const SizedBox(height: 16.0),
           TextField(
             controller: _deviceNameController,
-            decoration: const InputDecoration(labelText: 'Device Name'),
+            decoration: const InputDecoration(labelText: 'Name Device'),
+          ),
+          const SizedBox(height: 16.0),
+          // Display a dropdown menu of available Bluetooth devices or "No devices found"
+          DropdownButtonFormField<ScanResult>(
+            value: _selectedDevice.isNotEmpty
+                ? _availableDevices
+                .firstWhere(
+                    (device) => device.device.id.toString() == _selectedDevice,
+                orElse: () => _availableDevices.first)
+                : null,
+            items: filteredDevices.map((device) {
+              return DropdownMenuItem<ScanResult>(
+                value: device,
+                child: Text(device.device.name),
+              );
+            }).toList(),
+            onChanged: _availableDevices.isEmpty
+                ? null
+                : (device) {
+              setState(() {
+                _selectedDevice = device!.device.id.toString();
+              });
+            },
+            hint: _availableDevices.isEmpty
+                ? const Text('No devices found')
+                : const Text('Select Device (Optional)'),
+            disabledHint: const Text('No devices found'),
           ),
         ],
       ),
@@ -86,7 +67,6 @@ class _DeviceNameDialogState extends State<DeviceNameDialog> {
         TextButton(
           child: const Text('Cancel'),
           onPressed: () {
-            FlutterBlue.instance.stopScan();
             Navigator.of(context).pop();
           },
         ),
@@ -94,17 +74,97 @@ class _DeviceNameDialogState extends State<DeviceNameDialog> {
           child: const Text('Save'),
           onPressed: () {
             String deviceName = _deviceNameController.text.trim();
-            if (deviceName.isEmpty && _selectedDevice != null) {
-              deviceName = _selectedDevice!.name ?? '';
+            if (deviceName.isEmpty && _selectedDevice.isNotEmpty) {
+              deviceName = _availableDevices
+                  .firstWhere((device) => device.device.id.toString() == _selectedDevice,
+                  orElse: () => _availableDevices.first)
+                  .device
+                  .name;
             }
             if (deviceName.isNotEmpty) {
               widget.onDeviceNameEntered(deviceName);
-              FlutterBlue.instance.stopScan();
               Navigator.of(context).pop();
             }
           },
         ),
       ],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _requestLocationPermissionAndStartScanning();
+  }
+
+  @override
+  void dispose() {
+    _stopScanning();
+    super.dispose();
+  }
+
+  Future<void> _requestLocationPermissionAndStartScanning() async {
+    if (await Permission.location.isDenied) {
+      await Permission.location.request();
+    }
+
+    bool isAvailable = await FlutterBlue.instance.isAvailable;
+    bool isOn = await FlutterBlue.instance.isOn;
+
+    if (isAvailable && isOn) {
+      _startScanning();
+    } else {
+      _showBluetoothAlert(isAvailable, isOn);
+    }
+  }
+
+  void _startScanning() {
+    setState(() {
+      _isScanning = true;
+    });
+
+    FlutterBlue.instance.scanResults.listen((results) {
+      setState(() {
+        _availableDevices = results;
+      });
+    });
+
+    FlutterBlue.instance.startScan(timeout: const Duration(seconds: 10));
+  }
+
+  void _stopScanning() {
+    FlutterBlue.instance.stopScan();
+    setState(() {
+      _isScanning = false;
+    });
+  }
+
+  void _showBluetoothAlert(bool isAvailable, bool isOn) {
+    String title = 'Bluetooth Error';
+    String message = '';
+
+    if (!isAvailable) {
+      message = 'Bluetooth is not available on this device.';
+    } else if (!isOn) {
+      message = 'Bluetooth is turned off. Please turn it on and try again.';
+    }
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
