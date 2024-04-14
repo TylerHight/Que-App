b97d6cac66af020b72d9d9432e1e9ed095d6ce00
TylerHight <highttyler@gmail.com>
2024-04-13 21:16:30 -0500

    started bluetooth.dart

--- lib/bluetooth.dart
+++ lib/bluetooth.dart
@@ -1,11 +1,49 @@
-// bluetooth.dart
-///
-/// Bluetooth Operations
-///
-/// This file contains the implementation of Bluetooth operations for the app.
-/// It includes functionalities such as connecting to devices, sending and
-/// receiving data over Bluetooth, and managing Bluetooth-related tasks.
-///
-/// Author: Tyler Hight
-///
+import 'package:flutter_blue/flutter_blue.dart';
 
+class BLEController {
+  static late BluetoothDevice _device;
+  static late BluetoothCharacteristic _fan1Characteristic;
+  static bool _isConnected = false;
+
+  static Future<void> connectToDevice() async {
+    final FlutterBlue flutterBlue = FlutterBlue.instance;
+
+    // Start scanning for devices
+    flutterBlue.scan(timeout: Duration(seconds: 4)).listen((scanResult) {
+      // Connect to the device when found
+      _device = scanResult.device;
+      _device.connect().then((value) {
+        // Discover services and characteristics
+        _device.discoverServices().then((services) {
+          for (BluetoothService service in services) {
+            service.characteristics.forEach((characteristic) {
+              // Check for characteristics you're interested in
+              if (characteristic.uuid.toString() == '19B10001-E8F2-537E-4F6C-D104768A1214') {
+                _fan1Characteristic = characteristic;
+                _isConnected = true;
+              }
+              // Add more if needed for other characteristics
+            });
+          }
+        });
+      });
+    });
+  }
+
+  static Future<void> disconnect() async {
+    if (_isConnected) {
+      await _device.disconnect();
+      _isConnected = false;
+    }
+  }
+
+  static Future<void> turnOnFan1() async {
+    if (_isConnected) {
+      // Send command to turn on fan1
+      await _fan1Characteristic.write([1]); // Assuming 1 means turn on
+    }
+  }
+
+// Add more functions for other commands as needed
+
+}
