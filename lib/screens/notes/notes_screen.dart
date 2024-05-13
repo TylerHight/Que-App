7a9f1cd917312c656a93b13361f68a0dc61728cf
TylerHight <highttyler@gmail.com>
2024-05-13 01:54:31 -0500

    notes are displayed on notes_screen.dart

--- lib/screens/notes/notes_screen.dart
+++ lib/screens/notes/notes_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:que_app/models/note.dart';
+import 'package:que_app/models/notes_list.dart';
 
 class NotesScreen extends StatelessWidget {
   @override
@@ -6,7 +9,7 @@ class NotesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-            'Notes',
+          'Notes',
           style: TextStyle(color: Colors.white),
         ),
         backgroundColor: Colors.blue,
@@ -14,15 +17,43 @@ class NotesScreen extends StatelessWidget {
           IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
-              // Handle your logic here
+              // Handle adding a new note
             },
           ),
         ],
       ),
-      body: Center(
-        child: Text(
-          'Notes Screen',
-        ),
+      body: Consumer<NotesList>(
+        builder: (context, notesList, _) {
+          // Check if there are notes in the list
+          if (notesList.notes.isEmpty) {
+            // If no notes, display a message
+            return Center(
+              child: Text(
+                'No notes available',
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          } else {
+            // If there are notes, display them using ListView.builder
+            return ListView.builder(
+              itemCount: notesList.notes.length,
+              itemBuilder: (context, index) {
+                final note = notesList.notes[index];
+                return ListTile(
+                  title: Text(note.content),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Device: ${note.device?.deviceName ?? "N/A"}'),
+                      Text('Created on: ${note.creationDate.toLocal().toString().split('.')[0]}'),
+                    ],
+                  ),
+                  // Add any other relevant information you want to display
+                );
+              },
+            );
+          }
+        },
       ),
     );
   }
