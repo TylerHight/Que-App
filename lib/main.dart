9cf146f37c66cd105ba5f31f1721fc3987f474e0
TylerHight <highttyler@gmail.com>
2024-12-17 17:37:57 -0600

    restored appbar at bottom and fixed gradle error

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
-import 'core/database/migrations/migration_service.dart';
 import 'core/services/database_service.dart';
 import 'core/services/ble/ble_service.dart';
 import 'features/device_control/bloc/device_control_bloc.dart';
+import 'features/device_control/views/device_control_screen.dart';
+import 'features/notes/views/notes_screen.dart';
 import 'package:que_app/core/models/notes_list.dart';
 
-import 'features/device_control/views/device_control_screen.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Initialize the database
-  await MigrationService.initializeDatabase();
-
-  // Initialize the database service
-  final databaseService = DatabaseService.instance;
-
-  runApp(MyApp(databaseService: databaseService));
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final DatabaseService databaseService;
-
-  const MyApp({
-    super.key,
-    required this.databaseService,
-  });
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         Provider<DatabaseService>.value(
-          value: databaseService,
+          value: DatabaseService.instance,
         ),
         Provider<BleService>(
           create: (_) => BleService(),
@@ -58,8 +44,52 @@ class MyApp extends StatelessWidget {
         ),
         initialRoute: '/',
         routes: {
-          '/': (context) => const DeviceControlScreen(),
+          '/': (context) => const MyHomePage(),
         },
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static final List<Widget> _widgetOptions = <Widget>[
+    const DeviceControlScreen(),
+    const NotesScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_remote),
+            label: 'Control',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.edit_note),
+            label: 'Notes',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
     );
   }
